9994f06e5a653cfdfdf0e720369ff5e99731353f
Leonardo A <leo.adriano1994@gmail.com>
2019-08-04 19:43:27 -0300

    Add morgan to log

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const morgan = require("morgan");
+
 const app = require("./src/app");
+
+if (process.env.NODE_ENV !== "production") app.use(morgan("combined"));
 
 app.listen(process.env.APP_PORT || 3000);
